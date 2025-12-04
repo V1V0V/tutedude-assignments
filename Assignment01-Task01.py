5a6954c73bf1d8acfd6f963772efffca0b7f186f
V1V0V <100783321+V1V0V@users.noreply.github.com>
2025-12-04 18:14:04 +0530

    Add files via upload

---
+++ Assignment01-Task01.py
@@ -0,0 +1,8 @@
+x= input("Enter the first Number:")
+y= input("Enter the second Number:")
+print("Addition:", int(x) + int(y))
+print("Subtraction:", int(x) - int(y))
+print("Multiplication:", int(x) * int(y))
+print("Division:", int(x) / int(y))
+
+
