2f2b52dc78fef4b0daaa50e42039c53dac50273f
V1V0V <100783321+V1V0V@users.noreply.github.com>
2026-01-02 20:48:05 +0530

    Update Assignment01-Task01.py

--- Assignment01-Task01.py
+++ Assignment01-Task01.py
@@ -1,9 +1,10 @@
-x= input(int("Enter the first Number:"))
-y= input(int("Enter the second Number:"))
+x= int(input("Enter the first Number:"))
+y= int(input("Enter the second Number:"))
 print("Addition:",x + y)
 print("Subtraction:",x - y)
 print("Multiplication:",x*y)
 print("Division:",x/y)
+
 
 
 
